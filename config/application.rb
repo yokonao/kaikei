6356a22bf41079b138366735256c6df0baa57b01
yokonao <yokotukanao@gmail.com>
2025-09-12 16:11:41 +0900

    タイムゾーンを Asia/Tokyo に、ロケールを日本語にする

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ Bundler.require(*Rails.groups)
 
 module Kaikei
   class Application < Rails::Application
+    config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.yml").to_s]
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
