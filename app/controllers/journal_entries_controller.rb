e5919fa344b15c97b425c55a47add53b2d302385
yokonao <yokotukanao@gmail.com>
2025-09-18 16:24:51 +0900

    仕訳削除の実装

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -2,4 +2,10 @@ class JournalEntriesController < ApplicationController
   def index
     @journal_entries = JournalEntry.includes(:journal_entry_lines).order(entry_date: :desc, id: :desc)
   end
+
+  def destroy
+    @journal_entry = JournalEntry.find(params[:id])
+    @journal_entry.destroy
+    redirect_to journal_entries_path, notice: "仕訳が削除されました。"
+  end
 end
