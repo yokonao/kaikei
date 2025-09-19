040c2be72a1b2f717bae9df1a871271e55e27f26
yokonao <yokotukanao@gmail.com>
2025-09-19 13:21:47 +0900

    明細行削除ボタンと追加ボタンを削除したことに伴って不要になった JS コードを削除する

--- app/javascript/controllers/journal_entry_controller.js
+++ app/javascript/controllers/journal_entry_controller.js
@@ -1,54 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["container", "line", "debitContainer", "creditContainer", "debitTotal", "creditTotal", "difference"]
-
-  addDebitLine(event) {
-    event.preventDefault()
-    const template = this.getLineTemplate('debit')
-    this.debitContainerTarget.insertAdjacentHTML('beforeend', template)
-  }
-
-  addCreditLine(event) {
-    event.preventDefault()
-    const template = this.getLineTemplate('credit')
-    this.creditContainerTarget.insertAdjacentHTML('beforeend', template)
-  }
-
-  getLineTemplate(side) {
-    const timestamp = new Date().getTime()
-    const randomId = Math.random().toString(36).substring(2)
-    const fieldName = `journal_entry[journal_entry_lines_attributes][${timestamp}_${randomId}]`
-
-    // ERB側で事前生成されたテンプレートを使用
-    const templateElement = document.getElementById(`journal-entry-line-template`)
-    if (templateElement) {
-      return templateElement.innerHTML
-        .replace(/__FIELD_NAME_PLACEHOLDER__/g, fieldName)
-        .replace(/__SIDE_PLACEHOLDER__/g, side)
-    }
-
-    return ''
-  }
-
-  removeLine(event) {
-    event.preventDefault()
-
-    const line = event.target.closest('[data-journal-entry-target="line"]')
-    if (line) {
-      line.remove()
-    }
-  }
-
-  markForDestroy(event) {
-    const line = event.target.closest('[data-journal-entry-target="line"]')
-    if (event.target.checked) {
-      line.style.opacity = '0.5'
-    } else {
-      line.style.opacity = '1'
-    }
-  }
-
   updateAccountId(event) {
     const accountNameInput = event.target
     const line = accountNameInput.closest('[data-journal-entry-target="line"]')
