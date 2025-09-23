6a0a131368c04def067a855241fdf87d81661e76
yokonao <yokotukanao@gmail.com>
2025-09-23 09:34:43 +0900

    ユーザー認証のために必要な users, sessions テーブルを新規作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_17_075422) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_22_221040) do
   create_table "accounts", primary_key: "name", id: :string, force: :cascade do |t|
     t.integer "category", default: 0, null: false
     t.boolean "is_standard", null: false
@@ -36,6 +36,24 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_17_075422) do
     t.index ["journal_entry_id"], name: "index_journal_entry_lines_on_journal_entry_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
   add_foreign_key "journal_entry_lines", "accounts", column: "account_name", primary_key: "name"
   add_foreign_key "journal_entry_lines", "journal_entries", on_delete: :cascade
+  add_foreign_key "sessions", "users"
 end
