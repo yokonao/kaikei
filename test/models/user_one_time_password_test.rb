0caf57235c575ee42d8efa23943d2de5eae6bde2
yokonao <yokotukanao@gmail.com>
2025-09-24 14:03:16 +0900

    UserOneTimePassword.authenticate_otp のテスト追加

--- test/models/user_one_time_password_test.rb
+++ test/models/user_one_time_password_test.rb
@@ -40,4 +40,51 @@ class UserOneTimePasswordTest < ActiveSupport::TestCase
     assert_not one_time_password.valid?
     assert_includes one_time_password.errors.full_messages, "有効期限を入力してください"
   end
+
+  class AuthenticateOTPTest < ActiveSupport::TestCase
+    test "should return true and destroy the record for a valid OTP" do
+      user = users(:one)
+      otp = "123456"
+      one_time_password = user.user_one_time_passwords.create!(
+        password: otp,
+        expires_at: 1.hour.from_now
+      )
+
+      assert_difference("UserOneTimePassword.count", -1) do
+        assert UserOneTimePassword.authenticate_otp(user.id, otp)
+      end
+      assert_not UserOneTimePassword.authenticate_otp(user.id, otp) # The second time the authentication fails
+    end
+
+    test "should return false for a non-existent OTP" do
+      user = users(:one)
+      assert_not UserOneTimePassword.authenticate_otp(user.id, "123456")
+    end
+
+    test "should return false for an invalid OTP" do
+      user = users(:one)
+      otp = "123456"
+      one_time_password = user.user_one_time_passwords.create!(
+        password: otp,
+        expires_at: 1.hour.from_now
+      )
+
+      assert_no_difference("UserOneTimePassword.count") do
+        assert_not UserOneTimePassword.authenticate_otp(user.id, "wrong-otp")
+      end
+    end
+
+    test "should return false for an expired OTP" do
+      user = users(:one)
+      otp = "123456"
+      one_time_password = user.user_one_time_passwords.create!(
+        password: otp,
+        expires_at: 1.hour.ago
+      )
+
+      assert_no_difference("UserOneTimePassword.count") do
+        assert_not UserOneTimePassword.authenticate_otp(user.id, otp)
+      end
+    end
+  end
 end
