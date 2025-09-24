f0241e6c742c7b6f590f91e4f41c607bc96a519f
yokonao <yokotukanao@gmail.com>
2025-09-24 10:12:12 +0900

    User モデルにバリデーションと単体テストを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   has_one :user_basic_password, dependent: :destroy
   has_many :sessions, dependent: :destroy
 
+  validates :email_address, presence: true,
+            uniqueness: { message: "は登録済みです" },
+            length: { maximum: 254 }, # @see https://www.rfc-editor.org/errata/eid1690
+            format: { with: URI::MailTo::EMAIL_REGEXP, message: "の形式が正しくありません" }
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid with valid email address" do
+    user = User.new(email_address: "test@example.com")
+
+    assert_nothing_raised { user.validate! }
+  end
+
+  test "should be invalid wihtout email address" do
+    user = User.new
+
+    assert_not user.valid?
+    assert_includes user.errors.full_messages, "メールアドレスを入力してください"
+  end
+
+  test "should be invalid with too long email address" do
+    user = User.new(email_address: "#{"a" * 256}@example.com")
+
+    assert_not user.valid?
+    assert_includes user.errors.full_messages, "メールアドレスは254文字以内で入力してください"
+  end
+
+  test "should be invalid with invalid email address" do
+    user = User.new(email_address: "invalid")
+
+    assert_not user.valid?
+    assert_includes user.errors.full_messages, "メールアドレスの形式が正しくありません"
+  end
+
+  test "should be invalid with duplicated email address" do
+    email_address = "duplicate@example.com"
+    User.create(email_address: email_address)
+    user = User.new(email_address: email_address)
+
+    assert_not user.valid?
+    assert_includes user.errors.full_messages, "メールアドレスは登録済みです"
+  end
 end
