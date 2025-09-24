07e54cb8a29c8f6e308f0273998942112085a947
yokonao <yokotukanao@gmail.com>
2025-09-24 16:39:52 +0900

    session[:otp_user_id] をログイン後削除する

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,32 +10,11 @@ class SessionsController < ApplicationController
   def create
     case params[:login_method]
     when "otp"
-      user = User.find_by(email_address: params[:email_address])
-      fail_login and return unless user
-
-      session[:otp_user_id] = user.id
-      user.create_otp!
-
-      redirect_to new_session_path(login_method: "otp_verification"), notice: "ワンタイムパスワードを #{user.email_address} に送信しました。"
+      init_otp
     when "otp_verification"
-      user_id = session[:otp_user_id]
-      user = User.find_by(id: user_id)
-      fail_login and return unless user_id and user
-
-      if UserOneTimePassword.authenticate_otp(user_id, params[:otp])
-        success_login user
-      else
-        fail_login
-      end
+      verfiy_otp
     else
-      user = User.find_by(email_address: params[:email_address])
-      fail_login and return unless user
-
-      if UserBasicPassword.authenticate_by(user_id: user.id, password: params[:password])
-        success_login user
-      else
-        fail_login
-      end
+      basic_auth
     end
   end
 
@@ -45,6 +24,41 @@ class SessionsController < ApplicationController
   end
 
   private
+
+  def init_otp
+    user = User.find_by(email_address: params[:email_address])
+    fail_login and return unless user
+
+    session[:otp_user_id] = user.id
+    user.create_otp!
+
+    redirect_to new_session_path(login_method: "otp_verification"), notice: "ワンタイムパスワードを #{user.email_address} に送信しました。"
+  end
+
+  def verify_otp
+    user_id = session[:otp_user_id]
+    user = User.find_by(id: user_id)
+    fail_login and return unless user_id and user
+
+    if UserOneTimePassword.authenticate_otp(user_id, params[:otp])
+      success_login user
+    else
+      fail_login
+    end
+  ensure
+    session.delete(:otp_user_id)
+  end
+
+  def basic_auth
+    user = User.find_by(email_address: params[:email_address])
+    fail_login and return unless user
+
+    if UserBasicPassword.authenticate_by(user_id: user.id, password: params[:password])
+      success_login user
+    else
+      fail_login
+    end
+  end
 
   def success_login(user)
     start_new_session_for user
