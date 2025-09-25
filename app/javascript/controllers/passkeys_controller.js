e2fa3a065c7ab4dd53777f6e144e58d899292ff6
yokonao <yokotukanao@gmail.com>
2025-09-25 09:45:11 +0900

    passkey 追加の JS コードをリファクタ

--- app/javascript/controllers/passkeys_controller.js
+++ app/javascript/controllers/passkeys_controller.js
@@ -1,46 +1,47 @@
-// app/javascript/controllers/passkeys_controller.js
-import { Controller } from "@hotwired/stimulus"
-import { create } from '@github/webauthn-json'
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  register() {
-    fetch('/passkeys', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
-      },
-      body: JSON.stringify({
-        phase: "initiation"
-      })
-    })
-      .then(response => response.json())
-      .then(options => {
-        return create({ publicKey: options })
-      })
-      .then(credential => {
-        fetch('/passkeys', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
-          },
-          body: JSON.stringify({ ...credential, phase: "verification" })
-        })
-        .then(response => {
-          if (response.ok) {
-            window.location.reload()
-          } else {
-            response.json().then(data => {
-              console.error(data)
-            })
-            alert("Failed to register passkey")
-          }
-        })
-      })
-      .catch(error => {
-        console.error(error)
-        alert("Failed to register passkey")
-      })
+  async register() {
+    try {
+      const initiationResponse = await fetch("/passkeys", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
+            .content,
+        },
+        body: JSON.stringify({
+          phase: "initiation",
+        }),
+      });
+      const initiationResponseJSON = await initiationResponse.json();
+      const createCredentialOptions =
+        PublicKeyCredential.parseCreationOptionsFromJSON(
+          initiationResponseJSON
+        );
+      const credential = await navigator.credentials.create({
+        publicKey: createCredentialOptions,
+      });
+      const verificationResponse = await fetch("/passkeys", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')
+            .content,
+        },
+        body: JSON.stringify({ ...credential.toJSON(), phase: "verification" }),
+      });
+      if (verificationResponse.ok) {
+        window.location.reload();
+      } else {
+        // verificationResponse.json().then((data) => {
+        //   console.error(data);
+        // });
+        alert("Failed to register passkey");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Failed to register passkey");
+    }
   }
 }
