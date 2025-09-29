344f942a0c1bc0659f95615ef9ec1cb7d7eb682f
yokonao <yokotukanao@gmail.com>
2025-09-29 11:40:54 +0900

    ルーティング定義からパスワード関連のルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,4 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-
-  get "signup", to: "users#new"
-  resources :users, only: [ :create ]
-  resource :user, only: [ :show ]
-  namespace :users do
-    resources :passkeys, only: [ :create ]
-    resource :password, only: [ :update ]
-  end
-  resources :passwords, param: :token
-
-  resources :companies, only: [ :index, :new, :create ]
-
-  resource :session
-  resource :company_selection, only: [ :create ]
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -22,6 +6,20 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
+  get "signup", to: "users#new"
+  resources :users, only: [ :create ]
+  resource :user, only: [ :show ]
+  namespace :users do
+    resources :passkeys, only: [ :create ]
+  end
+
+  resources :companies, only: [ :index, :new, :create ]
+
+  resource :session
+  resource :company_selection, only: [ :create ]
 
   resources :journal_entries, only: [ :index, :new, :create, :edit, :update, :destroy ]
   resource :profit_and_loss, only: [ :show ]
