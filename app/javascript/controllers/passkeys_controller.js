d80f6e99ac95bea390bcdf50e6b0d67114499a37
yokonao <yokotukanao@gmail.com>
2025-09-29 13:05:14 +0900

    パスキー登録時のキャンセルエラーをハンドリング

--- app/javascript/controllers/passkeys_controller.js
+++ app/javascript/controllers/passkeys_controller.js
@@ -38,20 +38,29 @@ export default class extends Controller {
         // verificationResponse.json().then((data) => {
         //   console.error(data);
         // });
-        showErrorToast("Failed to register passkey");
+        showErrorToast("パスキーの登録に失敗しました。");
       }
     } catch (e) {
-      if (
-        e.name === "InvalidStateError" &&
-        e.message.includes("already registered")
-      ) {
-        showErrorToast(
-          "指定された認証器は既に登録されています。他の認証器を選択してください。"
-        );
-        return;
+      if (e instanceof DOMException) {
+        if (e.name === "NotAllowedError") {
+          // ユーザーがパスキーの登録を許可しなかった、またはキャンセルしたことを示すため何もしない
+          // https://www.w3.org/TR/webauthn-2/#sctn-privacy-considerations-client
+          return;
+        }
+
+        if (
+          e.name === "InvalidStateError" &&
+          e.message.includes("already registered")
+        ) {
+          showErrorToast(
+            "指定された認証器は既に登録されています。他の認証器を選択してください。"
+          );
+          return;
+        }
       }
+
       // console.error(e);
-      showErrorToast("Failed to register passkey");
+      showErrorToast("パスキーの登録に失敗しました。");
     }
   }
 }
