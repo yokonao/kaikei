ea9f2cd0135c84014e60ba22faf1a8f69af88d57
yokonao <yokotukanao@gmail.com>
2025-09-29 11:13:28 +0900

    Current.company が返す値はユーザーと関連付けがあることを保証する

--- app/models/current.rb
+++ app/models/current.rb
@@ -1,5 +1,10 @@
 class Current < ActiveSupport::CurrentAttributes
   attribute :session
   delegate :user, to: :session, allow_nil: true
-  delegate :company, to: :session, allow_nil: true
+
+  def company
+    return nil unless user
+
+    user.companies.find_by(id: session.company_id)
+  end
 end
