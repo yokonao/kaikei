d5e5edb45df6d6550fc9ca48222fcc54822864f0
yokonao <yokotukanao@gmail.com>
2025-10-02 20:24:33 +0900

    財務二表の表示期間自動設定ロジックに会計期間モデルを利用する

--- app/controllers/balance_sheets_controller.rb
+++ app/controllers/balance_sheets_controller.rb
@@ -1,18 +1,20 @@
 class BalanceSheetsController < ApplicationController
   def show
+    company = Current.company
     @from = (Date.parse(params[:from]) rescue nil)
     @to = (Date.parse(params[:to]) rescue nil)
 
     if @from.nil? && @to.nil?
-      @from = fiscal_year_start
-      @to = fiscal_year_end(@from)
+      period = AccountingPeriod.from_date(Date.current, start_month: company.accounting_period_start_month)
+      @from = period.start_date
+      @to = period.end_date
     elsif @from.nil?
       @from = @to.years_ago(1).days_since(1)
     elsif @to.nil?
       @to = @from.years_since(1).days_ago(1)
     end
 
-    bs = BalanceSheet.new(Current.company, @from, @to)
+    bs = BalanceSheet.new(company, @from, @to)
     bs.load!
 
     @asset_lines = bs.asset_lines
@@ -21,20 +23,5 @@ class BalanceSheetsController < ApplicationController
     @total_assets = bs.total_assets
     @total_liabilities = bs.total_liabilities
     @total_equity = bs.total_equity
-  end
-
-  private
-
-  # TODO: 将来的には会計年度の開始月をカスタマイズできるようにする
-  def fiscal_year_start(today = Date.current)
-    if today.month < 4
-      Date.new(today.year - 1, 4, 1)
-    else
-      Date.new(today.year, 4, 1)
-    end
-  end
-
-  def fiscal_year_end(today = Date.current)
-    fiscal_year_start(today).years_since(1).days_ago(1)
   end
 end
--- app/controllers/profit_and_losses_controller.rb
+++ app/controllers/profit_and_losses_controller.rb
@@ -1,11 +1,13 @@
 class ProfitAndLossesController < ApplicationController
   def show
+    company = Current.company
     @from = (Date.parse(params[:from]) rescue nil)
     @to = (Date.parse(params[:to]) rescue nil)
 
     if @from.nil? && @to.nil?
-      @from = fiscal_year_start
-      @to = fiscal_year_end(@from)
+      period = AccountingPeriod.from_date(Date.current, start_month: company.accounting_period_start_month)
+      @from = period.start_date
+      @to = period.end_date
     elsif @from.nil?
       @from = @to.years_ago(1).days_since(1)
     elsif @to.nil?
@@ -20,20 +22,5 @@ class ProfitAndLossesController < ApplicationController
     @total_revenue = pl.total_revenue
     @total_expenses = pl.total_expenses
     @net_income = pl.net_income
-  end
-
-  private
-
-  # TODO: 将来的には会計年度の開始月をカスタマイズできるようにする
-  def fiscal_year_start(today = Date.current)
-    if today.month < 4
-      Date.new(today.year - 1, 4, 1)
-    else
-      Date.new(today.year, 4, 1)
-    end
-  end
-
-  def fiscal_year_end(start_date)
-    start_date.years_since(1).days_ago(1)
   end
 end
