690ecf3c690c621e26be9c73feb7f977d0e4d780
yokonao <yokotukanao@gmail.com>
2025-10-02 17:07:17 +0900

    会計年度の開始月をモデルレベルでバリデーションする

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,5 @@ class Company < ApplicationRecord
   has_many :journal_entries
 
   validates :name, presence: true
+  validates :fy_start_month_num, inclusion: { in: (1..12), message: "は1月から12月のいずれかを指定してください" }
 end
--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -2,13 +2,37 @@ require "test_helper"
 
 class CompanyTest < ActiveSupport::TestCase
   test "should be valid with valid attributes" do
-    company = Company.new(name: "テスト会社")
+    company = Company.new(name: "テスト会社", fy_start_month_num: 4)
     assert_nothing_raised { company.validate! }
   end
 
   test "should be invalid wihtout name" do
-    company = Company.new
+    company = Company.new(fy_start_month_num: 4)
     assert_not company.valid?
     assert_includes company.errors.full_messages, "事業所名を入力してください"
+  end
+
+  test "should be invalid without fy_start_month_num" do
+    company = Company.new(name: "テスト会社")
+    assert_not company.valid?
+    assert_includes company.errors.full_messages, "会計年度の開始月は1月から12月のいずれかを指定してください"
+  end
+
+  test "should be invalid with zero fy_start_month_num" do
+    company = Company.new(name: "テスト会社", fy_start_month_num: 0)
+    assert_not company.valid?
+    assert_includes company.errors.full_messages, "会計年度の開始月は1月から12月のいずれかを指定してください"
+  end
+
+  test "should be invalid with negative fy_start_month_num" do
+    company = Company.new(name: "テスト会社", fy_start_month_num: -1)
+    assert_not company.valid?
+    assert_includes company.errors.full_messages, "会計年度の開始月は1月から12月のいずれかを指定してください"
+  end
+
+  test "should be invalid with too big fy_start_month_num" do
+    company = Company.new(name: "テスト会社", fy_start_month_num: 13)
+    assert_not company.valid?
+    assert_includes company.errors.full_messages, "会計年度の開始月は1月から12月のいずれかを指定してください"
   end
 end
