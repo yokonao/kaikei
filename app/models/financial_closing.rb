bb5c2fcf7e328d7198f045e417c76a4f8ed4b97b
yokonao <yokotukanao@gmail.com>
2025-10-04 19:00:38 +0900

    決算処理モデルの現在の状態を enum で定義する

--- app/models/financial_closing.rb
+++ app/models/financial_closing.rb
@@ -1,3 +1,9 @@
 class FinancialClosing < ApplicationRecord
   belongs_to :company
+
+  enum :status, {
+    adjusting: 1, # 決算整理仕訳を行うフェーズ
+    closing: 2, # 帳簿の締め切りを行うフェーズ
+    done: 3 # 決算処理完了を意味する
+  }
 end
