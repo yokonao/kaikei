9046dbf2ee2aecc5b98d03569da1b882dfb80ac4
yokonao <yokotukanao@gmail.com>
2025-10-05 06:21:40 +0900

    決算の開始日・終了日の詳細なバリデーションを追加する

--- app/models/financial_closing.rb
+++ app/models/financial_closing.rb
@@ -14,5 +14,28 @@ class FinancialClosing < ApplicationRecord
 
   def validate_start_and_end_date
     return unless start_date && end_date
+
+    if start_date >= end_date
+      errors.add(:base, "決算の終了日は開始日より後の日付を指定してください")
+      return
+    end
+
+    # 会社計算規則第59条2項（令和7年3月31日施行）
+    # https://laws.e-gov.go.jp/law/418M60000010013#Mp-Pa_3-Ch_1-Se_2-At_59
+    # 各事業年度に係る計算書類及びその附属明細書の作成に係る期間は、当該事業年度の前事業年度の末日の翌日（当該事業年度の前事業年度がない場合にあっては、成立の日）から当該事業年度の末日までの期間とする。
+    # この場合において、当該期間は、一年（事業年度の末日を変更する場合における変更後の最初の事業年度については、一年六箇月）を超えることができない。
+    if start_date + 18.months <= end_date
+      errors.add(:base, "決算期間は1年半以内にしなければいけません")
+      return
+    end
+
+    other_closings = FinancialClosing.where(company_id: company_id).order(end_date: :desc)
+    return if other_closings.empty?
+
+    latest_closing_end_date = other_closings.first.end_date
+
+    unless start_date == latest_closing_end_date + 1.day
+      errors.add(:base, "決算の開始日は前回決算の終了日の翌日にしてください")
+    end
   end
 end
--- test/models/financial_closing_test.rb
+++ test/models/financial_closing_test.rb
@@ -54,4 +54,81 @@ class FinancialClosingTest < ActiveSupport::TestCase
     assert_not fc.valid?
     assert_includes fc.errors.full_messages, "フェーズを入力してください"
   end
+
+  test "should be invalid with invalid date range" do
+    fc = FinancialClosing.new(
+      company: @company,
+      start_date: Date.new(2025, 3, 31),
+      end_date: Date.new(2025, 3, 31),
+      phase: :adjusting
+    )
+    assert_not fc.valid?
+    assert_includes fc.errors.full_messages, "決算の終了日は開始日より後の日付を指定してください"
+  end
+
+  test "should be invalid with too long date range" do
+    fc = FinancialClosing.new(
+      company: @company,
+      start_date: Date.new(2025, 4, 1),
+      end_date: Date.new(2026, 10, 1),
+      phase: :adjusting
+    )
+    assert_not fc.valid?
+    assert_includes fc.errors.full_messages, "決算期間は1年半以内にしなければいけません"
+  end
+
+  test "should be valid with 1.5 year date range" do
+    fc = FinancialClosing.new(
+      company: @company,
+      start_date: Date.new(2025, 4, 1),
+      end_date: Date.new(2026, 9, 30),
+      phase: :adjusting
+    )
+    assert_nothing_raised { fc.validate! }
+  end
+
+  test "should be valid with previous closing and correct start_date" do
+    FinancialClosing.create!(
+      company: @company,
+      start_date: Date.new(2023, 4, 1),
+      end_date: Date.new(2024, 3, 31),
+      phase: :adjusting
+    )
+    FinancialClosing.create!(
+      company: @company,
+      start_date: Date.new(2024, 4, 1),
+      end_date: Date.new(2025, 3, 31),
+      phase: :adjusting
+    )
+    fc = FinancialClosing.new(
+      company: @company,
+      start_date: Date.new(2025, 4, 1),
+      end_date: Date.new(2026, 3, 30),
+      phase: :adjusting
+    )
+    assert_nothing_raised { fc.validate! }
+  end
+
+  test "should be valid with previous closing and wrong start_date" do
+    FinancialClosing.create!(
+      company: @company,
+      start_date: Date.new(2023, 4, 1),
+      end_date: Date.new(2024, 3, 31),
+      phase: :adjusting
+    )
+    FinancialClosing.create!(
+      company: @company,
+      start_date: Date.new(2024, 4, 1),
+      end_date: Date.new(2025, 3, 31),
+      phase: :adjusting
+    )
+    fc = FinancialClosing.new(
+      company: @company,
+      start_date: Date.new(2025, 9, 1),
+      end_date: Date.new(2026, 8, 30),
+      phase: :adjusting
+    )
+    assert_not fc.valid?
+    assert_includes fc.errors.full_messages, "決算の開始日は前回決算の終了日の翌日にしてください"
+  end
 end
