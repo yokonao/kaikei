a697e1d41fefeff54ac3a26c04784429db47d0ab
yokonao <yokotukanao@gmail.com>
2025-10-04 19:05:00 +0900

    Company モデルに現在進行中の決算処理モデルを取得する処理を実装する

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,13 @@
 class Company < ApplicationRecord
   has_many :journal_entries
+  has_many :financial_closings
 
   validates :name, presence: true
   validates :accounting_period_start_month, inclusion: { in: (1..12), message: "は1月から12月のいずれかを指定してください" }
 
   attribute :accounting_period_start_month, :integer, default: 4
+
+  def ongoing_closing
+    financial_closings.where.not(phase: :done).take
+  end
 end
