19368f79f66f05c265485f824509420c53eb9816
yokonao <yokotukanao@gmail.com>
2025-10-08 13:38:48 +0900

    B/S に前期からの繰越分が計上されるようにする

--- app/models/balance_sheet.rb
+++ app/models/balance_sheet.rb
@@ -11,31 +11,46 @@ class BalanceSheet
   attr_reader :asset_lines, :total_assets, :liability_lines, :total_liabilities, :equity_lines, :total_equity
 
   def load!
+    asset_balances, liability_balances, equity_balances = Hash.new(0), Hash.new(0), Hash.new(0)
+    balance_forwards = BalanceForward.includes(:account).
+                                      where(company_id: company.id).
+                                      where(closing_date: start_date.yesterday)
+    balance_forwards.each do |bf|
+      case bf.account.category
+      when "asset"
+        asset_balances[bf.account_name] = bf.side == "credit" ? bf.amount : -bf.amount
+      when "liability"
+        liability_balances[bf.account_name] = bf.side == "debit" ? bf.amount : -bf.amount
+      when "equity"
+        equity_balances[bf.account_name] = bf.side == "debit" ? bf.amount : -bf.amount
+      end
+    end
+
     bs_lines = JournalEntryLine.joins(:journal_entry).
                                 includes(:account).
                                 where("journal_entry.company_id": company.id).
                                 where("journal_entry.entry_date": start_date..end_date).
                                 where("account.category": [ :asset, :liability, :equity ])
 
-    @asset_lines = bs_lines.
-                          filter { |line| line.account.asset? }.
-                          group_by { |line| line.account_name }.
-                          transform_values { |lines| lines.sum { |line| line.side == "debit" ? line.amount : -line.amount } }.
-                          map { |k, v| Line.new(name: k, amount: v) }
+    bs_lines.filter { |line| line.account.asset? }.
+             group_by { |line| line.account_name }.
+             transform_values { |lines| lines.sum { |line| line.side == "debit" ? line.amount : -line.amount } }.
+             each { |k, v| asset_balances[k] += v }
+    @asset_lines = asset_balances.map { |k, v| Line.new(name: k, amount: v) }.reject { |line| line.amount.zero? }
     @total_assets = @asset_lines.sum(&:amount)
 
-    @liability_lines = bs_lines.
-                          filter { |line| line.account.liability? }.
-                          group_by { |line| line.account_name }.
-                          transform_values { |lines| lines.sum { |line| line.side == "credit" ? line.amount : -line.amount } }.
-                          map { |k, v| Line.new(name: k, amount: v) }
+    bs_lines.filter { |line| line.account.liability? }.
+             group_by { |line| line.account_name }.
+             transform_values { |lines| lines.sum { |line| line.side == "credit" ? line.amount : -line.amount } }.
+             each { |k, v| liability_balances[k] += v }
+    @liability_lines = liability_balances.map { |k, v| Line.new(name: k, amount: v) }.reject { |line| line.amount.zero? }
     @total_liabilities = @liability_lines.sum(&:amount)
 
-    @equity_lines = bs_lines.
-                          filter { |line| line.account.equity? }.
-                          group_by { |line| line.account_name }.
-                          transform_values { |lines| lines.sum { |line| line.side == "credit" ? line.amount : -line.amount } }.
-                          map { |k, v| Line.new(name: k, amount: v) }
+    bs_lines.filter { |line| line.account.equity? }.
+             group_by { |line| line.account_name }.
+             transform_values { |lines| lines.sum { |line| line.side == "credit" ? line.amount : -line.amount } }.
+             each { |k, v| equity_balances[k] += v }
+    @equity_lines = equity_balances.map { |k, v| Line.new(name: k, amount: v) }.reject { |line| line.amount.zero? }
     @total_equity = @equity_lines.sum(&:amount)
 
     nil
