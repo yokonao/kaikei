84f4150f23fb9af2105ff4579bcfb12494c74922
yokonao <yokotukanao@gmail.com>
2025-10-13 07:03:26 +0900

    アカウント削除の単体テストを修正

--- test/models/concerns/user/incineration_test.rb
+++ test/models/concerns/user/incineration_test.rb
@@ -5,7 +5,6 @@ class User::IncinerationTest < ActiveSupport::TestCase
     # Setup
     user = User.create!(email_address: "test-for-incineration@example.com")
     company = companies(:company_one)
-    Membership.create!(user: user, company: company)
     Session.create!(user: user)
     user.create_otp!
     user.user_passkeys.create!(
@@ -18,6 +17,7 @@ class User::IncinerationTest < ActiveSupport::TestCase
     Rails.application.eager_load!
     models_to_delete = ApplicationRecord.descendants.
                                          reject(&:abstract_class?).
+                                         reject { |model| model == Membership }. # 事業所に所属しているユーザーは削除不可能という仕様があるのでここでは除外
                                          select { |model| model.column_names.include?("user_id") }
     assert_not_empty models_to_delete
 
@@ -32,5 +32,18 @@ class User::IncinerationTest < ActiveSupport::TestCase
     # Post-assertions
     assert user.destroyed?
     models_to_delete.each { |model| assert_empty model.where(user_id: user.id).to_a, "#{model} should be empty" }
+  end
+
+  test "should raise error when you try to delete a user who belongs to a company" do
+    user = User.create!(email_address: "test-for-incineration@example.com")
+    company = companies(:company_one)
+    Membership.create!(user: user, company: company)
+
+    incineration = User::Incineration.new(user)
+
+    e = assert_raises(ActiveModel::ValidationError) do
+      incineration.run
+    end
+    assert_includes e.message, "事業所に所属しているユーザーを削除することはできません"
   end
 end
