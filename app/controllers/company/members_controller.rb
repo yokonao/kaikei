6222f49a3e4e03d3b18f7358b458379f3b7b34b8
yokonao <yokotukanao@gmail.com>
2025-10-12 20:43:49 +0900

    事業所のメンバー一覧を表形式で表示する

--- app/controllers/company/members_controller.rb
+++ app/controllers/company/members_controller.rb
@@ -1,4 +1,6 @@
 class Company::MembersController < ApplicationController
   def index
+    company = Current.company
+    @members = User.includes(:memberships).where("memberships.company_id": company.id).page(params[:page]).per(100)
   end
 end
