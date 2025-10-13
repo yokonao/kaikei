eaa4427ea94b7292c74c3ca54df7022de380ffc7
yokonao <yokotukanao@gmail.com>
2025-10-13 16:47:15 +0900

    メンバー追加時に Invitation レコードの挿入とアクセス用のトークン発行を行う

--- app/controllers/company/members_controller.rb
+++ app/controllers/company/members_controller.rb
@@ -5,9 +5,18 @@ class Company::MembersController < ApplicationController
   end
 
   def create
+    user, company = Current.user, Current.company
     email_address = params[:email_address]
 
+    iv = Invitation.create!(
+      email_address: email_address,
+      inviter_email_address: user.email_address,
+      company: company
+    )
+    token = iv.generate_token_for(:invitation)
+
     Rails.logger.debug "TODO: メンバーの追加処理を実装する"
+    Rails.logger.debug "token: #{token}"
 
     redirect_to company_members_path
   end
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,8 @@
 class Invitation < ApplicationRecord
+  belongs_to :company
+
+  generates_token_for :invitation, expires_in: 1.days
+
   validates :email_address, presence: true,
           length: { maximum: 254 }, # @see https://www.rfc-editor.org/errata/eid1690
           format: { with: URI::MailTo::EMAIL_REGEXP, message: "の形式が正しくありません" }
