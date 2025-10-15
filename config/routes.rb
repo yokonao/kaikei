8be134544061f0d47067e83e1dff1cd33f05eb6a
yokonao <yokotukanao@gmail.com>
2025-10-15 20:08:10 +0900

    ルーティング定義をアルファベット順にソートする

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,6 @@ Rails.application.routes.draw do
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-  get "signup", to: "users#new"
-  resources :users, only: [ :show, :create, :destroy ] do
-    resources :passkeys, only: [ :create ]
-  end
-
   resources :companies, only: [ :index, :show, :new, :create, :update, :destroy ] do
     resources :financial_closings, only: [ :index, :edit, :create, :update ]
     resources :financial_statements, only: [ :show ]
@@ -22,8 +17,11 @@ Rails.application.routes.draw do
     resources :members, only: [ :index, :create, :destroy ]
   end
   resources :invitations, only: [ :show, :update ]
-
   resource :session, only: [ :new, :create, :update, :destroy ]
+  get "signup", to: "users#new"
+  resources :users, only: [ :show, :create, :destroy ] do
+    resources :passkeys, only: [ :create ]
+  end
 
   # Defines the root path route ("/")
   root "companies#index"
