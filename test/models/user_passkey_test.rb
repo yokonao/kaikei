f4f6c2a1d2080267e84683a3e3268e8425afbde2
yokonao <yokotukanao@gmail.com>
2025-10-16 20:33:34 +0900

    aaguid カラムの更新に伴い UserPasskey の単体テストを更新

--- test/models/user_passkey_test.rb
+++ test/models/user_passkey_test.rb
@@ -3,8 +3,28 @@ require "test_helper"
 class UserPasskeyTest < ActiveSupport::TestCase
   test "can create with valid attributes" do
     user = users(:one)
-    user_passkey = UserPasskey.new(id: SecureRandom.alphanumeric(24), user: user, display_name: "Chrome on Mac", public_key: "public_key", sign_count: 0)
+    user_passkey = UserPasskey.new(
+      id: SecureRandom.alphanumeric(24),
+      user: user,
+      display_name: "Passkey Name",
+      public_key: "public_key",
+      sign_count: 0,
+      aaguid: "00000000-0000-0000-0000-000000000000"
+    )
     assert_nothing_raised { user_passkey.save! }
+  end
+
+  test "can create without aaguid" do
+    user = users(:one)
+    user_passkey = UserPasskey.new(
+      id: SecureRandom.alphanumeric(24),
+      user: user,
+      display_name: "Passkey Name",
+      public_key: "public_key",
+      sign_count: 0,
+    )
+    assert_nothing_raised { user_passkey.save! }
+    assert_equal "00000000-0000-0000-0000-000000000000", user_passkey.aaguid
   end
 
   test "should be invalid wihtout id" do
