99162d0217262203aa23b2f7fe79edab3e81fcef
yokonao <yokotukanao@gmail.com>
2025-10-20 11:20:16 +0900

    エラーメッセージだけでなくお知らせメッセージも Toast UI として出せるようにした

--- app/javascript/utils/toast.js
+++ app/javascript/utils/toast.js
@@ -1,3 +1,16 @@
+export function showInfoToast(message) {
+  const template = document.querySelector('#info-toast-template');
+  const id = `info-toast-${Date.now()}-${Math.random().toString(36).slice(-8)}`;
+  const clone = template.content.cloneNode(true);
+
+  clone.querySelector("[popover]").id = id;
+  clone.querySelector("p").textContent = message;
+  clone.querySelector("button").setAttribute("popovertarget", id);
+
+  document.body.appendChild(clone);
+  document.getElementById(id).showPopover();
+}
+
 export function showErrorToast(message) {
   const template = document.querySelector('#error-toast-template');
   const id = `error-toast-${Date.now()}-${Math.random().toString(36).slice(-8)}`;
